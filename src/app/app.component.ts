acb8879cd053e0f0ea4b5f5413b511cb7b7e0ad9
JHades <jhades.dev@gmail.com>
2018-08-21 16:06:17 +0200

    Angular Core Deep Dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+
+    courses = COURSES;
 
 
 }
--- src/db-data.ts
+++ src/db-data.ts
@@ -18,8 +18,17 @@ export const COURSES: any = [
         category: 'BEGINNER',
         lessonsCount: 10
     },
+
     {
         id: 3,
+        description: 'NgRx In Depth',
+        longDescription: "Learn the modern Ngrx Ecosystem, including Store, Effects, Router Store, Ngrx Entity, Dev Tools and Schematics.",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-ngrx-course.png',
+        category: 'ADVANCED'
+    },
+
+    {
+        id: 4,
         description: "Angular for Beginners",
         iconUrl: 'https://angular-academy.s3.amazonaws.com/thumbnails/angular2-for-beginners-small-v2.png',
         longDescription: "Establish a solid layer of fundamentals, learn what's under the hood of Angular",
@@ -27,45 +36,38 @@ export const COURSES: any = [
         lessonsCount: 10
     },
     {
-        id: 4,
-        description: 'Angular Security Course - Web Security Fundamentals',
+        id: 5,
+        description: 'Angular Security Course',
         longDescription: "Learn Web Security Fundamentals and apply them to defend an Angular / Node Application from multiple types of attacks.",
         iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/security-cover-small-v2.png',
         category: 'ADVANCED',
         lessonsCount: 11
     },
     {
-        id: 5,
-        description: 'Angular PWA - Progressive Web Apps Course',
+        id: 6,
+        description: 'Angular PWA Course',
         longDescription: "<p class='course-description'>Learn Angular Progressive Web Applications, build the future of the Web Today.",
         iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-pwa-course.png',
         category: 'ADVANCED',
         lessonsCount: 8
     },
     {
-        id: 6,
-        description: 'Angular NgRx Store Reactive Extensions Architecture Course',
-        longDescription: "Learn how to the Angular NgRx Reactive Extensions and its Tooling to build a complete application.",
-        iconUrl: 'https://angular-academy.s3.amazonaws.com/thumbnails/ngrx-angular.png',
-        category: 'ADVANCED'
-    },
-    {
         id: 7,
-        description: 'Angular Advanced Library Laboratory: Build Your Own Library',
+        description: 'Angular Advanced Course',
         longDescription: "Learn Advanced Angular functionality typically used in Library Development. Advanced Components, Directives, Testing, Npm",
         iconUrl: 'https://angular-academy.s3.amazonaws.com/thumbnails/advanced_angular-small-v3.png',
         category: 'ADVANCED'
     },
     {
         id: 8,
-        description: 'The Complete Typescript Course',
+        description: 'Complete Typescript Course',
         longDescription: "Complete Guide to Typescript From Scratch: Learn the language in-depth and use it to build a Node REST API.",
         iconUrl: 'https://angular-academy.s3.amazonaws.com/thumbnails/typescript-2-small.png',
         category: 'BEGINNER'
     },
     {
         id: 9,
-        description: 'Rxjs and Reactive Patterns Angular Architecture Course',
+        description: 'Angular Architecture Course',
         longDescription: "Learn the core RxJs Observable Pattern as well and many other Design Patterns for building Reactive Angular Applications.",
         iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-academy/blog/images/rxjs-reactive-patterns-small.png',
         category: 'BEGINNER'
