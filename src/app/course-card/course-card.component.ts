8e4818d836e1b80bda8e44ddbcfd0561774d314c
Your Name <you@example.com>
2018-08-27 14:44:45 +0200

    Angular Core Deep Dive

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
