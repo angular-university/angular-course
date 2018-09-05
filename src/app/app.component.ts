2977e730639e1d93212b0d7d31c8a224227c171b
Your Name <you@example.com>
2018-09-05 15:10:28 +0200

    Angular Core Deep Dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
 
     courses = COURSES;
 
+    title = COURSES[0].description;
+
+    price = 9.992423424242;
+
+    rate = 0.67;
+
+    course = COURSES[0];
+
+    startDate = new Date(2000, 0, 1);
+
     onCourseSelected(course:Course) {
 
         console.log("App component", course);
