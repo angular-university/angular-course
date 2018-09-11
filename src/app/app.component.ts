df80e145da860719edc490331141bfc0bc9d093a
Your Name <you@example.com>
2018-09-11 15:11:39 +0200

    Angular Core Deep Dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,46 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
     courses = COURSES;
 
-    title = COURSES[0].description;
 
-    price = 9.992423424242;
+    @ViewChildren(CourseCardComponent, {read: ElementRef})
+    cards : QueryList<ElementRef>;
 
-    rate = 0.67;
 
-    course = COURSES[0];
+    constructor() {
 
-    startDate = new Date(2000, 0, 1);
+
+    }
+
+
+    ngAfterViewInit() {
+
+        console.log(this.cards);
+
+
+
+    }
+
+
+
 
     onCourseSelected(course:Course) {
 
-        console.log("App component", course);
+
+
 
     }
+
+
 
 }
