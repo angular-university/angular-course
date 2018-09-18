e3673f79f2e0828b09cd6247c9615c766eea00c2
Your Name <you@example.com>
2018-09-18 14:22:13 +0200

    Angular Core Deep Dive

--- src/db-data.ts
+++ src/db-data.ts
@@ -5,7 +5,7 @@ export const COURSES: any = [
     {
         id: 1,
         description: "Angular Core Deep Dive",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        //iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
         longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
         lessonsCount: 10
     },
