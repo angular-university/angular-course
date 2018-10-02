ff51a8111a3af555738c21810e11d9eeca500933
Your Name <you@example.com>
2018-10-02 15:39:55 +0200

    Angular Core Deep Dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +14,23 @@ import {Observable} from 'rxjs';
 export class AppComponent implements OnInit {
 
 
-  courses = COURSES;
+  courses;
 
-  constructor() {
+  constructor(private http: HttpClient) {
 
   }
 
   ngOnInit() {
+
+      const params = new HttpParams()
+          .set("page", "1")
+          .set("pageSize", "10");
+
+      this.http.get('/api/courses', {params})
+          .subscribe(
+              courses => this.courses = courses
+          );
+
   }
 
 
