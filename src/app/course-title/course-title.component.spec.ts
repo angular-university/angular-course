41ca5abc400c48bc61526177786c719b805ada13
JHades <jhades.dev@gmail.com>
2020-11-18 10:15:37 +0100

    upgrade to Angular 11 and Node 14

--- src/app/course-title/course-title.component.spec.ts
+++ src/app/course-title/course-title.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CourseTitleComponent } from './course-title.component';
 
@@ -6,7 +6,7 @@ describe('CourseTitleComponent', () => {
   let component: CourseTitleComponent;
   let fixture: ComponentFixture<CourseTitleComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CourseTitleComponent ]
     })
