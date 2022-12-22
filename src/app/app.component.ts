a08b4ed822cc2789a64838bc2e92f2da882327e4
a161120 <david.tarvin@physiciansmutual.com>
2022-12-22 09:45:05 -0600

    Angular Input and Output Decorators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,17 @@ import { Course } from './model/course';
 export class AppComponent {
 
   coreCourse = COURSES[0];
+  // the data from COURSES[0] populates coreCourse - same with otherse below
 
   rxjsCourse = COURSES[1];
 
   ngrxCourse = COURSES[2];
 
   onCourseSelected(course:Course) {
-    console.log("App component - click event bubbled...");
+
+    // what is being passed in as the argument is the $event from the child component TypeScript file, which is the object being emitted by that component's method.
+   
+    console.log("App component - click event bubbled...", course);
   }
 
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { COURSES } from 'src/db-data'
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { COURSES } from 'src/db-data';
 import { EventEmitter } from '@angular/core';
 import { Course } from "../model/course";
 
@@ -12,8 +12,14 @@ export class CourseCardComponent implements OnInit {
 
   @Input()
   course:Course;
+  // This property is used on the parent component's HTML template file and is in square brackets for property binding. The value for this comes from a property in the parent's 
+  // TypeScript file.
 
+
+  @Output()
   courseSelected = new EventEmitter<Course>();
+  // This property is used in the parent component to trigger a method in the parent component's TypeScript file to run when the event occurs. The object emitted to the parent component in 
+  // this case is of a type Course.
 
   constructor() { }
 
@@ -22,9 +28,12 @@ export class CourseCardComponent implements OnInit {
   }
 
   onCourseViewed() {
-    console.log("card component - button clicked ...");
+
+    console.log("card component - button clicked...");
 
     this.courseSelected.emit(this.course);
+    // emit is used to actuaally trigger the event handler on the parent component's HTML template file. In this case what is emitted is the specific course object that was clicked 
+    // on. emit is attached to the property that is the EventEmitter.
   }
 
 }
