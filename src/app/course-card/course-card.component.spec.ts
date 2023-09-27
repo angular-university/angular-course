4d3071eea01a4b963d49345c699db35528046f04
Victor Diaz <105568910+celestialmachine@users.noreply.github.com>
2023-09-27 05:42:47 +0000

    ng-u building our first angular component

---
+++ src/app/course-card/course-card.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CourseCardComponent } from './course-card.component';
+
+describe('CourseCardComponent', () => {
+  let component: CourseCardComponent;
+  let fixture: ComponentFixture<CourseCardComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CourseCardComponent]
+    });
+    fixture = TestBed.createComponent(CourseCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent {
+
+}
