f89d794c437e3d6f416549814081f19af4b1ddbf
vasco <jhades.dev@gmail.com>
2023-12-12 16:55:13 +0100

    Angular 17 recordings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,4 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, computed, DoCheck, Inject, Injector, OnInit, signal} from '@angular/core';
-import {Course} from './model/course';
-import {Observable} from 'rxjs';
-import {AppConfig, CONFIG_TOKEN} from './config';
-import {COURSES} from '../db-data';
-import {CoursesService} from './courses/courses.service';
-import {createCustomElement} from '@angular/elements';
-import {CourseTitleComponent} from './course-title/course-title.component';
+import {Component, computed, effect, EffectRef, signal} from '@angular/core';
 import {CourseCardComponent} from './courses/course-card/course-card.component';
 import {CourseImageComponent} from './courses/course-image/course-image.component';
 import {NgForOf} from '@angular/common';
@@ -34,10 +27,40 @@ export class AppComponent {
 
   });
 
+  effectRef: EffectRef;
+
+  constructor() {
+
+    this.effectRef = effect((onCleanup) => {
+
+      onCleanup(() => {
+
+
+        console.log(`Cleanup occurred!`);
+
+      });
+
+      const counterValue = this.counter();
+
+      const derivedCounterValue = this.derivedCounter();
+
+      console.log(` counter: ${counterValue} derived counter: ${derivedCounterValue}`);
+
+    },
+      {
+        manualCleanup: true
+      });
+
+  }
+
   increment() {
 
     this.counter.update(val => val + 1);
 
   }
 
+
+  onCleanup() {
+    this.effectRef.destroy();
+  }
 }
