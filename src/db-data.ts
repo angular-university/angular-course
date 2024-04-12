73e95f8dc7b1b9a3e9b2955d3f6e141df9243b9f
Leonardo Badell <leonardobadell@hotmail.com>
2024-04-11 22:43:35 -0300

    Adding if else syntax to the repository

--- src/db-data.ts
+++ src/db-data.ts
@@ -5,7 +5,7 @@ export const COURSES: any = [
     {
         id: 1,
         description: "Angular Core Deep Dive",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        // iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
         longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
         category: 'INTERMEDIATE',
         lessonsCount: 10
@@ -23,7 +23,7 @@ export const COURSES: any = [
         id: 3,
         description: 'NgRx In Depth',
         longDescription: "Learn the modern Ngrx Ecosystem, including Store, Effects, Router Store, Ngrx Entity, Dev Tools and Schematics.",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-ngrx-course.png',
+        // iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-ngrx-course.png',
         category: 'ADVANCED'
     },
 
