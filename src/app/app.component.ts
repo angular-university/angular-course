4d40a81d0574cec9f7112e4672b4f4e4da78c2b8
Leonardo Badell <leonardobadell@hotmail.com>
2024-04-28 18:49:48 -0300

    Adding changes to the branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
+import { CoursesService } from './services/courses.service';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +19,8 @@ export class AppComponent implements OnInit {
 
   // courses;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, 
+              private coursesService: CoursesService) {
 
   }
 
