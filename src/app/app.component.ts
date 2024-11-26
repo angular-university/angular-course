0323c945484416ab908cf6f42f124436dc12de66
Chidume Nnamdi <kurtwanger40@gmail.com>
2024-11-26 16:24:23 +0100

    Upgrade to Angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 export class AppComponent {
 
